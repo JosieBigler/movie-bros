97ed7c5e2f4ee7cb2b653deeed66fb759f264396
JosieBigler <jbigler01@hamline.edu>
2023-12-26 17:04:38 -0600

    Example routing finished

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,31 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Route,
-//   Link,
-// } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <div>
-//         <h1>Hello World</h1>
-//         <Link to="about">About Us</Link>
-//       </div>
-//     ),
-//   },
-//   {
-//     path: "about",
-//     element: <div>About</div>,
-//   },
-// ]);
+//Add pages here. Order matters.
+//https://reactrouter.com/en/main/start/tutorial
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>You don't belong here. Go away</div>
+  },
+  {
+    path: "test",
+    element: <div>We have tested.</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
