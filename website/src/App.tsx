aa516ab106a2716f2d6f156530dbdb696f48295d
AimLow <78690774+AimLow-SMM@users.noreply.github.com>
2023-12-26 23:36:25 -0600

    beep boop

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,7 @@
 import { getMovies } from './data/dataService';
 import { Movie } from './model/movie';
 import { MoviesComponent } from './ui/Movies';
-import Rate from './pages/Rate'
+import { Rate } from './pages/Rate'
 
 function App() {
 
--- website/src/pages/Rate.tsx
+++ website/src/pages/Rate.tsx
@@ -1,27 +1,40 @@
 
-const Rate = () => {
-  return <>
-    <div className="relative">
-      <img className="always-filled opacity-60" src='https://www.themoviedb.org/t/p/original/5C5oQtJ50Vg2qoVTp9XjNPyqZlq.jpg'/>
-      <div className="up-shadow"></div>
-      <div className="ttt">
-        <div className="ttt1">
-          <h2 className="tracking-wide text-4xl font-semibold text-slate-100 uppercase">Fortress</h2>
-        </div>
+import { useState } from "react";
+
+let imagesPath: string[] = [
+  'https://www.themoviedb.org/t/p/original/5C5oQtJ50Vg2qoVTp9XjNPyqZlq.jpg', 
+  'https://static.bunnycdn.ru/i/cache/images/d/d7/d7d38f0a99976c7f8baeb4a701977399.jpg'
+];
+let sss = "Fortress";
+let ssss = "1992";
+let sssss = "1h 35m";
+let ssssss = "Josie";
+
+let ddd = "Josie 10";
+let dddd = "Shawn 8.8";
+let ddddd = "Wesley 0.5";
+let dddddd = "Jasper 5";
+
+function Rate2() {
+  const [aaa, aaaa] = useState(imagesPath[0])
+  function sayHello() {
+    aaaa(aaa === imagesPath[0] ? imagesPath[1] : imagesPath[0])
+  }
+  return <img className="always-filled opacity-60" src={aaa} onClick={sayHello}/>
+  
+}
+export const Rate = () => {
+return <>
+  <div className="relative">
+    <Rate2></Rate2>
+    {/* <img className="always-filled opacity-60" src={jjj} onClick={sayHello}/> */}
+    {/* <div className="up-shadow"></div> */}
+    <div className="pointer-events-none details">
+      <div className="ttt1 relative">
+        <h2 className="tracking-wide text-4xl font-semibold uppercase">{sss}</h2>
+        <p className="[&>*]:pr-4"><span>{ssss}</span><span>{sssss}</span><span>{ssssss}</span></p>
       </div>
     </div>
-    {/* <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img className="rounded-t-lg" src='https://www.themoviedb.org/t/p/original/bneUTWFAcVCdsb0O5UwZbJd8xqZ.jpg' alt="" />
-      </a>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">fsdffdff</h5>
-        </a>
-        <p className="mb-3 font-bold text-red-700 dark:text-gray-400">asdasdasd</p>
-      </div>
-    </div> */}
-  </>;
-};
-  
-  export default Rate;
+    <div className="the-bros"><span>{ddd}</span><span>{dddd}</span><span>{ddddd}</span><span>{dddddd}</span></div>
+  </div>
+</>;};
