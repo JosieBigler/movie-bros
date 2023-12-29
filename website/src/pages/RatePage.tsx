1ac5c839b44504720b808e92de9ae94b559e2a03
AimLow <78690774+AimLow-SMM@users.noreply.github.com>
2023-12-29 03:18:39 -0600

    Bros bubbles

--- website/src/pages/RatePage.tsx
+++ website/src/pages/RatePage.tsx
@@ -4,17 +4,30 @@ import { useState } from "react";
 
 let imagesPath: string[] = [
   'https://www.themoviedb.org/t/p/original/5C5oQtJ50Vg2qoVTp9XjNPyqZlq.jpg', 
-  'https://static.bunnycdn.ru/i/cache/images/d/d7/d7d38f0a99976c7f8baeb4a701977399.jpg'
+  'https://www.themoviedb.org/t/p/original/tPu7eaHFcWCewPuL9ULMC9AoHPr.jpg'
 ];
 let sss = "Fortress";
 let ssss = "1992";
 let sssss = "1h 35m";
 let ssssss = "Josie";
 
-let ddd = "Josie 10";
-let dddd = "Shawn 8.8";
-let ddddd = "Wesley 0.5";
-let dddddd = "Jasper 5";
+let ddd = "User name";
+let dddd = "User rating";
+
+interface Bro{
+  name: string,
+  rating: number
+}
+const Bros : Bro[] = [
+  {
+    name: "Jasper",
+    rating: 5.5
+  },
+  {
+    name: "Josie",
+    rating: 10
+  },
+]
 
 const testApiCall = () => {
   axios({
@@ -47,6 +60,19 @@ return <>
         <p className="[&>*]:pr-4"><span>{ssss}</span><span>{sssss}</span><span>{ssssss}</span></p>
       </div>
     </div>
-    <div className="the-bros"><span>{ddd}</span><span>{dddd}</span><span>{ddddd}</span><span>{dddddd}</span></div>
+    <Rate3 bros={Bros}></Rate3>
   </div>
 </>;};
+
+const Rate3 : React.FC<{bros : Bro[]}> = ({bros})  => {
+  return (
+    <div className="the-bros">
+      <span><span>{ddd}</span><span>{dddd}</span></span>
+      {
+        bros.map(x => {
+          return <span><span>{x.name}</span><span>{x.rating}</span></span>
+        })
+      }
+    </div>
+  )
+}
