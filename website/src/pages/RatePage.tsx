0fc21f3583d5c6f096377f1e49e6346deb81931e
Josie Bigler <jbigler01@hamline.edu>
2024-01-07 21:50:24 -0600

    Three useEffects to capture state changes

--- website/src/pages/RatePage.tsx
+++ website/src/pages/RatePage.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, FormEvent, FormEventHandler, useEffect, useState } from "r
 import apiService from "../data/ApiService";
 import { RatingApiResponseDTO, RatingResponseDTO } from "../data/RatingApiResponseDTO";
 import HubConnector from "../data/HubConnector";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 const imagesPath: string[] = [
   'https://www.themoviedb.org/t/p/original/5C5oQtJ50Vg2qoVTp9XjNPyqZlq.jpg', 
@@ -60,55 +61,97 @@ const Ratings  = ({movieId} : ratingProps)  => {
   const [haveRated, setHaveRated] = useState(false);
   const [rating, setRating] = useState(0);
   const [avgUser, setAvgUser] = useState<RatingResponseDTO>({ userName: "Average", rating: -1, movieId});
-  const { newMessage, events } = HubConnector();
+  const [connectionRef, setConnection] = useState < HubConnection > ();
+  const [newRating, setNewRating] = useState<RatingResponseDTO | null>(null);
+
+
+  const createHubConnection = () => {
+    const con = new HubConnectionBuilder()
+      .withUrl('https://localhost:7097/hubs/rating')
+      .withAutomaticReconnect()
+      .build();
+    setConnection(con);
+  };
+
+  useEffect(() => {
+    if(newRating){
+      console.log('newRating effect', ratings);
+      if(!ratings.some(x => x.movieId == newRating.movieId && x.userName == newRating.userName))
+      {
+        setRatings(prevState => [...prevState, newRating]);
+      }
+      
+    }
+    return () => {
+      setNewRating(null);
+    }
+
+  }, [ratings, newRating]);
+
+  useEffect(() => {
+    if (connectionRef) {
+      try {
+        connectionRef
+          .start()
+          .then(() => {
+            connectionRef.on('messageReceived', (movieId : stirng, userName : string, rating : number) => {
+              console.log(movieId, userName, rating);
+              setNewRating({ movieId, userName, rating});
+              //do rating stuff here 
+            });
+          })
+          .catch((err) => {
+            console.error(`Error: ${err}`);
+          });
+      } catch (error) {
+        console.error(error as Error);
+      }
+    }
+
+    return () => {
+      connectionRef?.stop();
+    };
+  }, [connectionRef]);
 
   useEffect(() => {
     //on first page load, do the stuff
+    createHubConnection();
     apiService.getIdentity();
 
     
     const fetchData = async () => {
-      console.log(movieId);
+      console.log('fetch data run');
 
       const identity = await apiService.getIdentity();
       const data = await apiService.getMovieRatings(movieId);
       setRatings([...data.data]);
       setAverage();
-      //SetTheAverageRatingOfRatings([avgUser, ...data.data]); // I don't know what JavaScript is doing but it works
       setHaveRated(data.haveRated); 
       setUserName(identity.data);
       
     }
 
     fetchData().catch(console.error);
-    
-    events((movieId, userName, rating) => {
-      let alreadyRated = ratings.find(x => x.movieId == movieId && x.userName == userName);
-      if(alreadyRated) return; 
-
-      setRatings(prevState => [...prevState, {movieId, userName, rating }]);
-    });
-
-
   }, []);
 
 
   const rateMovie = async () => {
+    console.log('rate movie fired');
+    const rate = { userName, rating, movieId}
+    setHaveRated(true);
+    await apiService.rateMovie(movieId, rate.rating);
 
     //update internal state. 
-    const rate = { userName, rating, movieId}
-    //setRatings(prevState => [...prevState, rate]);
+    setRatings(prevState => [...prevState, rate]);
     //const newAverage = SetTheAverageRatingOfRatings(ratings); // I don't know what JavaScript is doing but it works
     //setAvgUser({ userName: 'Average', rating: newAverage, movieId});
     //setAverage();
     
     //send to database.
-    apiService.rateMovie(movieId, rate.rating);
     
     //send to Hub. 
-    newMessage(rate);
+    connectionRef.send("newMessage", movieId, userName, rating);
 
-    setHaveRated(true);
 
   }
 
@@ -133,7 +176,9 @@ const Ratings  = ({movieId} : ratingProps)  => {
     <div className="the-bros">
       {haveRated ? 
         <div>
+          <RateBubble key={avgUser.userName} DisplayName={avgUser.userName} RatingValue={avgUser.rating}></RateBubble>
         {
+          
           ratings?.map(x => {
             // return <span  key={x.userName}><span>{x.userName}: </span><span>{x.rating}</span></span>
             return <RateBubble key={x.userName} DisplayName={x.userName} RatingValue={x.rating}></RateBubble>
